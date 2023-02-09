51574c129e46b20f17cc109014a723fd2d81dbf4
BertieWoo88 <050688V@gmail.com>
2023-02-09 10:33:04 +0300

    4 example random added

--- Example_4/Program.cs
+++ Example_4/Program.cs
@@ -1,3 +1,5 @@
-﻿double numerA = 22;
-double numberB = 5;
-Console.WriteLine(numerA/numberB);
+﻿double numberA = new Random().Next(1,10); //1-9
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,10); //1-9
+Console.WriteLine(numberB);
+Console.WriteLine(numberA/numberB);
