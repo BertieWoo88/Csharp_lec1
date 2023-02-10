cabfc238d1004eefe83544b11d6559b1a3054d4c
BertieWoo88 <050688V@gmail.com>
2023-02-10 08:59:43 +0300

    examp 7 friend and dog added

---
+++ Example_7/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача про двух друзей и собаку
+double distance = 10000, time = 0;
+int firstFSpeed = 2, secondFSpeed = 1, dogSpeed = 5,
+count = 1, friend = 2;
+while(distance>10)
+{
+    if(friend==1)
+    {
+        time = distance/(firstFSpeed+dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance/(dogSpeed-secondFSpeed);
+        friend = 1; 
+    }
+    distance = distance - (firstFSpeed-secondFSpeed)*time;
+    count++;
+}
+Console.WriteLine(count);
+ 
